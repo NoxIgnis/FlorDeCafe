cca01f23aa7c96fbab0f52e1c083e33a1df7e5ed
HenriqueMsjr <henrique.junior@madatech.com.br>
2024-03-07 08:54:26 -0300

    ajustes no scss

--- flor_cafe/src/app/components/header/header.component.ts
+++ flor_cafe/src/app/components/header/header.component.ts
@@ -1,14 +1,19 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'nav-bar',
   standalone: true,
   imports: [NgOptimizedImage, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  @Input() login: boolean = false;
 
+  constructor(private router: Router) {
+    this.login = this.router.url == '/login'; // Atribui a URL atual a uma variável
+  }
 }
